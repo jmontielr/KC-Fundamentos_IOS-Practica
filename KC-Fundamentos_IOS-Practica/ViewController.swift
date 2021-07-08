39387ff84fffb6af4e77fed7b9cbae8d629012a2
Javier Montiel <jmr@jmontielr.com>
2021-07-08 12:18:38 +0200

    Initial Commit

---
+++ KC-Fundamentos_IOS-Practica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KC-Fundamentos_IOS-Practica
+//
+//  Created by Fco. Javier Montiel Rivero on 8/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
