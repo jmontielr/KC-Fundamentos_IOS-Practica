5c583787cded759a887af9349bfba920293fa0c9
Javier Montiel <jmr@jmontielr.com>
2021-07-11 07:40:55 +0200

    20210711_0740_After loading splash

---
+++ KC-Fundamentos_IOS-Practica/Domain/Models/hero.swift
@@ -0,0 +1,15 @@
+//
+//  hero.swift
+//  KC-Fundamentos_IOS-Practica
+//
+//  Created by Fco. Javier Montiel Rivero on 11/7/21.
+//
+
+import Foundation
+
+struct SuperNaturalPerson {
+    var image: String? = nil
+    var nickname: String
+    var description: String
+    var score: Float = 0.0
+}
