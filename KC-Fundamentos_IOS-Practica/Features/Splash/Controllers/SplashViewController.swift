cd50faaf90ab6037bf64e5b4967222f40786174c
Javier Montiel <jmr@jmontielr.com>
2021-07-11 08:36:14 +0200

    20210711_0835_SplashViewController

---
+++ KC-Fundamentos_IOS-Practica/Features/Splash/Controllers/SplashViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SplashViewController.swift
+//  KC-Fundamentos_IOS-Practica
+//
+//  Created by Fco. Javier Montiel Rivero on 11/7/21.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+    
+    // MARK: - IOOutlets
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        activityIndicator.startAnimating()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        activityIndicator.stopAnimating()
+    }
+    
+    // MARK: - Private methods
+    private func loadData(){
+        //Simula la carga de datos en otro hilo auxiliar al main
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1)){
+            print("Load data completed")
+        }
+    }
+}
